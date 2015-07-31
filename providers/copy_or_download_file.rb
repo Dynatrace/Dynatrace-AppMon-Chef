f5e3b77d9d111222530ea35f78ad84031b0b4b52
Martin Etmajer <m.etmajer@gmail.com>
2015-07-31 14:33:19 +0200

    Give artifacts in files folder preference over downloading.

--- providers/copy_or_download_file.rb
+++ providers/copy_or_download_file.rb
@@ -6,16 +6,6 @@
 #
 
 action :run do
-  remote_file "Download file from #{new_resource.file_url} to #{new_resource.path}" do
-    source new_resource.file_url unless new_resource.file_url.nil?
-    path   new_resource.path
-    owner  new_resource.dynatrace_owner
-    group  new_resource.dynatrace_group
-    mode   '0644'
-    action :create_if_missing
-    only_if { !::File.exist?(new_resource.path) }
-  end
-
   cookbook_file "Copy file #{new_resource.file_name} to #{new_resource.path}" do
     source new_resource.file_name
     path   new_resource.path
@@ -23,6 +13,16 @@ action :run do
     group  new_resource.dynatrace_group
     mode   '0644'
     ignore_failure true
+    action :create_if_missing
+    only_if { run_context.has_cookbook_file_in_cookbook?(cookbook_name, new_resource.file_name) && !::File.exist?(new_resource.path) }
+  end
+
+  remote_file "Download file from #{new_resource.file_url} to #{new_resource.path}" do
+    source new_resource.file_url unless new_resource.file_url.nil?
+    path   new_resource.path
+    owner  new_resource.dynatrace_owner
+    group  new_resource.dynatrace_group
+    mode   '0644'
     action :create_if_missing
     only_if { !::File.exist?(new_resource.path) }
   end
