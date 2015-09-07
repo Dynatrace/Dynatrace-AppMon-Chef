f310971c85d03ac9c6a595c5e2f6f269e3fe904b
Martin Etmajer <m.etmajer@gmail.com>
2015-09-07 20:18:42 +0200

    Remove dependency on PostgreSQL for testing.

--- Berksfile
+++ Berksfile
@@ -7,6 +7,5 @@ cookbook 'java'
 
 group :integration do
   cookbook 'apache2'
-  cookbook 'dynatrace_pwh_postgresql', path: 'test/fixtures/cookbooks/dynatrace_pwh_postgresql'
 end
 
--- test/fixtures/cookbooks/dynatrace_pwh_postgresql/metadata.rb
+++
@@ -1,6 +0,0 @@
-name             'dynatrace_pwh_postgresql'
-description      'Installs a Dynatrace Performance Warehouse in PostgreSQL'
-version          '1.0.0'
-
-depends          'database'
-depends          'postgresql'
