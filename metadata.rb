9cb9dd5347e13e30ceafe96cf41d6c3eb0681b57
Martin Etmajer <m.etmajer@gmail.com>
2016-03-04 13:53:59 +0100

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'martin.etmajer@dynatrace.com'
 license 'MIT'
 description 'Installs the Dynatrace Application Monitoring solution.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.13'
+version '6.3.0'
 source_url 'https://github.com/Dynatrace/Dynatrace-Chef'
 issues_url 'https://github.com/Dynatrace/Dynatrace-Chef/issues'
 
