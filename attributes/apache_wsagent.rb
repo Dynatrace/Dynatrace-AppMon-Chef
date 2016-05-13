af090f9e5c78c31c3fb67d12c008ce1ba9736319
Piotr Ozieblo <piotr.ozieblo@dynatrace.com>
2016-05-13 13:25:38 +0100

    Revert "[JLT-150550] Converted apache_wsagent to LWRP style, hanged default apache conf file location"
    This reverts commit 8ba779ec471f420924cc53f8d092840592c580a9.
    
    Conflicts:
    	metadata.rb
    	recipes/apache_wsagent.rb
    	resources/java_agent.rb

--- attributes/apache_wsagent.rb
+++ attributes/apache_wsagent.rb
@@ -6,11 +6,7 @@
 #
 
 # The path to the Apache HTTP Server's config file.
-default['dynatrace']['apache_wsagent']['apache']['config_file_path'] = '/etc/httpd/conf/httpd.conf'
+default['dynatrace']['apache_wsagent']['apache']['config_file_path'] = '/etc/apache2/apache2.conf'
 
 # The path to the Dynatrace Agent library.
 default['dynatrace']['apache_wsagent']['linux']['agent_path'] = '/opt/dynatrace/agent/lib64/libdtagent.so'
-
-# The Apache HTTP daemon name.
-default['dynatrace']['apache_wsagent']['linux']['apache_daemon'] = 'httpd'
-
