e5b74c7c1f9a53db8de974d7582c3ed4c6cf450c
Jacek Kulesz <jacek.kulesz@dynatrace.com>
2016-06-03 10:56:35 +0200

    JLT-151905 Add host agent to dynatrace_demo.

--- providers/run_tar_installer_for_hostagent.rb
+++ providers/run_tar_installer_for_hostagent.rb
@@ -23,16 +23,18 @@ action :run do
     block do
 		installation_path_part = get_folder_name(install_dir)
 		
-		log '#####: modyfy #{install_dir}/#{installation_path_part}/agent/conf/dthostagent.ini  host_agent_name=' + new_resource.host_agent_name + '  collector=' + new_resource.host_agent_collector + " :#####"
+		puts '#####: modify ' + install_dir + '/' + installation_path_part + '/agent/conf/dthostagent.ini  host_agent_name=' + new_resource.host_agent_name + '  collector=' + new_resource.host_agent_collector + " :#####"
 		modify_ini_file("#{install_dir}/#{installation_path_part}/agent/conf/dthostagent.ini", new_resource.host_agent_name, new_resource.host_agent_collector)
+		
 		exec_cmd("cp #{install_dir}/#{installation_path_part}/init.d/dynaTraceHostagent /etc/init.d/")
 		# remove init.d from tmp folder
-    exec_cmd("rm -rf #{install_dir}/#{installation_path_part}//init.d")
+    exec_cmd("rm -rf #{install_dir}/#{installation_path_part}/init.d")
 
     installation_path = "#{new_resource.installer_prefix_dir}/#{installation_path_part}"
 		exec_cmd(cp_install_dir_cmd(::File.dirname(new_resource.installer_path) << "/#{installation_path_part}", new_resource.installer_prefix_dir))
+
  		exec_cmd(get_chown_recursively_cmd(installation_path, new_resource.dynatrace_owner, new_resource.dynatrace_group))
-		link "Create a symlink of the #{new_resource.name} installation to #{new_resource.installer_prefix_dir}/dynatrace" do
+		link "Create a symlink of the #{installation_path} installation to #{new_resource.installer_prefix_dir}/dynatrace" do
 			target_file "#{new_resource.installer_prefix_dir}/dynatrace"
 			to "#{installation_path}"
 		end
@@ -77,8 +79,6 @@ def exec_cmd(cmd2exec)
 end
 
 def modify_ini_file(ini_file, lhost_agent_name, lhost_agent_collector)
-  
-  ruby_block "modify_ini_file #{ini_file}" do
     
 #	fileArray = []
 #	open(ini_file).each { |x| 
@@ -97,10 +97,11 @@ def modify_ini_file(ini_file, lhost_agent_name, lhost_agent_collector)
 #		f.puts(fileArray)
 #	end
 
-    block do 
-      Dynatrace::Helpers.file_append_or_replace_line(ini_file, "Name host", 'Name ' + lhost_agent_name)
-      Dynatrace::Helpers.file_append_or_replace_line(ini_file, "Server localhost", 'Server ' + lhost_agent_collector)
-    end
-  end
+  Dynatrace::Helpers.file_append_or_replace_line(ini_file, "Name host", 'Name ' + lhost_agent_name)
+  Dynatrace::Helpers.file_append_or_replace_line(ini_file, "Server localhost", 'Server ' + lhost_agent_collector)
+#  open(ini_file).each { |x| 
+#    line = x.gsub("\n",'')
+#    puts '#####: ' + line
+#  }
   
 end
