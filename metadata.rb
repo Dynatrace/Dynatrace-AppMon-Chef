d3d0016aa042303c4b0386cb2d8ef82a6fe72c75
Jacek Kulesz <jacek.kulesz@dynatrace.com>
2016-08-04 11:43:29 +0200

    JLT-157880 Latest EasyTravel sprint build should be used on dynasprint. - Used: http://artifactory.emea.cpwr.corp/artifactory/simple/demoability-release-local/com/dynatrace/easytravel/dynatrace-easytravel-linux-x86_64/2.0.0.2455/dynatrace-easytravel-linux-x86_64-2.0.0.2455.jar URL.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'martin.etmajer@dynatrace.com'
 license 'MIT'
 description 'Installs the Dynatrace Application Monitoring solution.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '6.5.0'
+version '15.0'
 source_url 'https://github.com/Dynatrace/Dynatrace-Chef'
 issues_url 'https://github.com/Dynatrace/Dynatrace-Chef/issues'
 
@@ -16,17 +16,31 @@ depends 'apache2'
 depends 'java'
 
 recipe 'dynatrace', 'Installs the Dynatrace Server and Agents package.'
+
 recipe 'dynatrace:agents_package', 'Installs the Dynatrace Agents package.'
+recipe 'dynatrace:agents_package_uninstall', 'Uninstalls the Dynatrace Agents package.'
+
 recipe 'dynatrace:apache_wsagent', 'Installs the Dynatrace WebServer Agent for the Apache HTTP Server.'
+recipe 'dynatrace:apache_wsagent_uninstall', 'Uninstalls the Dynatrace WebServer Agent for the Apache HTTP Server.'
+
 recipe 'dynatrace:collector', 'Installs the Dynatrace Collector.'
-recipe 'dynatrace:dotnet_agent', 'Installs the Dynatrace .NET Agent.'
+recipe 'dynatrace:collector_uninstall', 'Uninstalls the Dynatrace Collector.'
+
 recipe 'dynatrace:dynatrace_user', 'Installs the Dynatrace system user.'
+recipe 'dynatrace:dynatrace_user_uninstall', 'Uninstalls the Dynatrace system user.'
+
+recipe 'dynatrace:server', 'Installs the Dynatrace Server.'
+recipe 'dynatrace:server_uninstall', 'Uninstalls the Dynatrace Server.'
+
+recipe 'dynatrace:wsagent_package', 'Installs the Dynatrace WebServer Agent package.'
+recipe 'dynatrace:wsagent_package_uninstall', 'Uninstalls the Dynatrace WebServer Agent package.'
+
+recipe 'dynatrace:easy_travel', 'Installs the Easy Travel.'
+recipe 'dynatrace:easy_travel_uninstall', 'Uninstalls the Easy Travel.'
+
+recipe 'dynatrace:dotnet_agent', 'Installs the Dynatrace .NET Agent.'
 recipe 'dynatrace:iis_wsagent', 'Installs the Dynatrace WebServer Agent for the Microsoft IIS Server.'
 recipe 'dynatrace:java_agent', 'Installs the Dynatrace Agent Java Agent.'
 recipe 'dynatrace:memory_analysis_server', 'Installs the Dynatrace Memory Analysis Server.'
-recipe 'dynatrace:server', 'Installs the Dynatrace Server.'
 recipe 'dynatrace:server_license', 'Installs the Dynatrace Server License.'
-recipe 'dynatrace:wsagent_package', 'Installs the Dynatrace WebServer Agent package.'
-recipe 'dynatrace:easy_travel', 'Installs the Easy Travel.'
-recipe 'dynatrace:easy_travel_uninstall', 'Uninstalls the Easy Travel.'
 recipe 'dynatrace:upgrade_system', 'Upgrades OS.'
