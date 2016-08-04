0a258e737eef9b9d9ee536a797be9ecf773dbaf5
Jacek Kulesz <jacek.kulesz@dynatrace.com>
2016-08-04 11:58:37 +0200

    JLT-157833 Apache bootstrap agent version doesn't match.

--- recipes/apache_wsagent_uninstall.rb
+++ recipes/apache_wsagent_uninstall.rb
@@ -13,12 +13,60 @@ apache_config_file_path = node['dynatrace']['apache_wsagent']['apache']['config_
 apache_daemon = node['dynatrace']['apache_wsagent']['linux']['apache_daemon']
 
 if platform_family?('debian', 'fedora', 'rhel')
-  if not apache_daemon.empty?
-    service apache_daemon do
-      action [:stop, :disable]
-    end
-  end
+  arch = node['dynatrace']['apache_wsagent']['arch']
+  agent_path = node['dynatrace']['apache_wsagent']['linux'][arch]['agent_path']
+  node.set['dynatrace']['apache_wsagent']['agent_path'] = agent_path
 else
-  raise "Unsupported platform"
+  log "Unsupported platform family." do
+    level :warn
+  end
 end
 
+fileExists = apache_config_file_path
+if File.exist?(fileExists)
+  ruby_block "Remove the #{name} from Apache HTTPD's config file #{apache_config_file_path}" do
+    block do
+      # Host Agent is already installed
+      search_pattern = "LoadModule dtagent_module"
+      line_to_remove = "#{search_pattern} \"#{agent_path}\""
+      
+      puts "Search pattern: #{search_pattern}"
+      puts "Line to remove: #{line_to_remove}"
+      
+      Dynatrace::Helpers.file_replace_line(apache_config_file_path, line_to_remove, '')
+    end
+    if not apache_daemon.empty?
+      notifies :restart, "service[#{apache_daemon}]", :immediately
+    end
+    ignore_failure true
+  end
+end
+
+apache_config_file_path = '/opt/easytravel/resources/custom_httpd.conf'
+fileExists = apache_config_file_path
+if File.exist?(fileExists)
+  ruby_block "Remove the #{name} from Apache HTTPD's config file #{apache_config_file_path}" do
+    block do
+        # Host Agent is already installed
+        search_pattern = "LoadModule dtagent_module"
+        line_to_remove = "#{search_pattern} \"#{agent_path}\""
+        
+        puts "Search pattern: #{search_pattern}"
+        puts "Line to remove: #{line_to_remove}"
+        
+        Dynatrace::Helpers.file_replace_line(apache_config_file_path, line_to_remove, '')
+    end
+    if not apache_daemon.empty?
+      notifies :restart, "service[#{apache_daemon}]", :immediately
+    end
+    ignore_failure true
+  end
+end
+
+# We only state here that such a daemon already exists. We do this to
+# make the notification mechanism work (see above). 
+ if not apache_daemon.empty?
+  service apache_daemon do
+    action :nothing
+  end
+end
