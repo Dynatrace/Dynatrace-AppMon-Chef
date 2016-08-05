ba1a8aba77064d6eba79ab32ca8aa241a7b51513
Piotr Ozieblo <piotr.ozieblo@dynatrace.com>
2016-08-05 09:20:17 +0200

    [JLT-155202] Fixed upload/download from/to S3 using IAM role.

--- libraries/s3_helpers.rb
+++ libraries/s3_helpers.rb
@@ -2,10 +2,16 @@ module Dynatrace
   module S3Helpers
     # NOTE: a "require 'aws-sdk'" should be placed before calling this method
     def self.init_s3_client(region, access_key_id, secret_access_key)
-      credentials = Aws::Credentials.new(access_key_id, secret_access_key)
-      shared_credentials = Aws::SharedCredentials.new
-      if (access_key_id.nil? and secret_access_key.nil? and shared_credentials.loadable?)
-        credentials = shared_credentials
+      if (access_key_id.nil? and secret_access_key.nil?)
+        shared_credentials = Aws::SharedCredentials.new
+        if (shared_credentials.loadable?)
+          credentials = shared_credentials
+        else
+          # Assume IAM role credentials
+          credentials = Aws::InstanceProfileCredentials.new
+        end
+      else
+        credentials = Aws::Credentials.new(access_key_id, secret_access_key)
       end
       s3 = Aws::S3::Client.new(region: region, credentials: credentials)
     end
