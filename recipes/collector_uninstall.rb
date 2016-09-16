cbd2daa757bd6cba65428cea26e992c3d8f99038
Piotr Ozieblo <piotr.ozieblo@dynatrace.com>
2016-09-16 20:08:33 +0200

    [JLT-162467] dynatrace cookbook cleanup * Cleanup of collector_uninstall recipe. * Added kitchen test * Verified using collector-uninstall-centos-6

--- recipes/collector_uninstall.rb
+++ recipes/collector_uninstall.rb
@@ -1,41 +1,20 @@
 #
 # Cookbook Name:: dynatrace
-# Recipes:: collector
+# Recipes:: collector_uninstall
 #
-# Copyright 2015, Dynatrace
+# Copyright 2016, Dynatrace
 #
 include_recipe 'dynatrace::node_info'
 include_recipe 'java'
 
 name = 'Uninstall Dynatrace Collector'
 
-installer_bitsize = node['dynatrace']['collector']['installer']['bitsize']
-
-agent_port = node['dynatrace']['collector']['agent']['port']
-
-server_hostname = node['dynatrace']['collector']['server']['hostname']
-server_port     = node['dynatrace']['collector']['server']['port']
-
-collector_jvm_xmx           = node['dynatrace']['collector']['jvm']['xmx']
-collector_jvm_xms           = node['dynatrace']['collector']['jvm']['xms']
-collector_jvm_perm_size     = node['dynatrace']['collector']['jvm']['perm_size']
-collector_jvm_max_perm_size = node['dynatrace']['collector']['jvm']['max_perm_size']
-
-dynatrace_owner = node['dynatrace']['owner']
-dynatrace_group = node['dynatrace']['group']
-
 if platform_family?('debian', 'fedora', 'rhel')
   installer_prefix_dir = node['dynatrace']['collector']['linux']['installer']['prefix_dir']
-  installer_file_name  = node['dynatrace']['collector']['linux']['installer']['file_name']
-  installer_file_url   = node['dynatrace']['collector']['linux']['installer']['file_url']
-
   installer_cache_dir = "#{Chef::Config['file_cache_path']}/dynatrace"
-  installer_path      = "#{installer_cache_dir}/#{installer_file_name}"
-
   service = 'dynaTraceCollector'
-  init_scripts = [service]
 else
-  # Unsupported
+  raise "Unsupported platform family."
 end
 
 service "#{name}" do
@@ -55,8 +34,3 @@ dynatrace_delete_directory_by_link "#{link2del}" do
   link2delete link2del
 end
 
-#directory "Delete the installation directory #{link2del}" do
-#  path      link2del
-#  recursive true
-#  action    :delete
-#end
