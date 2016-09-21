ecdf1eb0578345387699da10ae2952522bf5e6cf
Piotr Ozieblo <piotr.ozieblo@dynatrace.com>
2016-09-21 13:03:33 +0200

    [JLT-162467] dynatrace cookbook cleanup * cleanup of java_agent recipe * removed platform check

--- recipes/java_agent.rb
+++ recipes/java_agent.rb
@@ -2,17 +2,13 @@
 # Cookbook Name:: dynatrace
 # Recipes:: java_agent
 #
-# Copyright 2015, Dynatrace
+# Copyright 2015-2016, Dynatrace
 #
 
 include_recipe "dynatrace::agents_package"
 
 dynatrace_java_agent node['dynatrace']['java_agent']['name'] do
-  if platform_family?('debian', 'fedora', 'rhel')
-    arch = node['dynatrace']['java_agent']['arch']
-    agent_path node['dynatrace']['java_agent']['linux'][arch]['agent_path']
-  else
-    # Unsupported
-  end
+  arch = node['dynatrace']['java_agent']['arch']
+  agent_path node['dynatrace']['java_agent']['linux'][arch]['agent_path']
 end
 
