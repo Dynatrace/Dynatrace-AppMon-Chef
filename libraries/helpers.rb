bdc796e09665a528edab4c0b2280f18eabb2087b
Piotr Ozieblo <piotr.ozieblo@dynatrace.com>
2016-09-22 15:16:13 +0200

    https://downloads.dynatracesaas.com/updates/dynaTrace-6.3.4.1034.zip[JLT-162467] dynatrace cookbook cleanup * Minor cleanup (mostly indentation fixes)

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -171,22 +171,21 @@ EOH
     end
 
     def self.wait_until_port_is_open(port, timeout = 120, ip = '127.0.0.1', continue_exec = nil)
-      
       time_begin = Time.now           # Current time
       #puts ">> wait_until_port_is_open IP=#{ip}:#{port} timeout=#{timeout}"
-        Timeout.timeout(timeout, DynatraceTimeout) do
-      while !self.port_is_open?(ip, port) do
-        sleep(1)
+      Timeout.timeout(timeout, DynatraceTimeout) do
+        while !self.port_is_open?(ip, port) do
+          sleep(1)
+        end
+        time_end = Time.now           # Current time
+        diff = (time_end - time_begin).ceil
+        #puts ">> wait_until_port_is_open - waited=#{diff} seconds"
       end
-      time_end = Time.now           # Current time
-      diff = (time_end - time_begin).ceil
-      #puts ">> wait_until_port_is_open - waited=#{diff} seconds"
-    end
     rescue DynatraceTimeout
-      if continue_exec.nil? 
+      if continue_exec.nil?
         raise DynatraceNotReady.new("#{ip}:#{port}", timeout)
       else
-        pyts "!!!!! ERROR:timeout! wait_until_port_is_open IP=#{ip}:#{port} timeout=#{timeout}"
+        # puts "!!!!! ERROR:timeout! wait_until_port_is_open IP=#{ip}:#{port} timeout=#{timeout}"
       end
     end
 
--- recipes/node_info.rb
+++ recipes/node_info.rb
@@ -1,44 +1,51 @@
-  #for AWS it can be:
-  node_platform = node['platform']              # "amazon"
-  node_platform_version = node['platform_version']      # "2016.03"
-  node_os = node['os']                    # "linux"
-  node_os_version = node['os_version']            # "4.4.5-15.26.amzn1.x86_64"
-  node_kernel_machine = node['kernel']['machine']       # "x86_64"
-  node_ipaddress = node['ipaddress']
+#
+# Cookbook Name:: dynatrace
+# Recipes:: node_info
+#
+# Copyright 2016, Dynatrace
+#
 
-  ec2 = node['ec2']
-  if !ec2.nil?
-    node_public_hostname = node['ec2']['public_hostname']
-    node_public_ipv4 = node['ec2']['public_ipv4']
-    node_local_ipv4 = node['ec2']['local_ipv4']
-  else
-    node_public_hostname = nil
-    node_public_ipv4 = nil
-    node_local_ipv4 = nil
-  end
+                                                  # for EC2 instance it can be:
+node_platform = node['platform']                  # "amazon"
+node_platform_version = node['platform_version']  # "2016.03"
+node_os = node['os']                              # "linux"
+node_os_version = node['os_version']              # "4.4.5-15.26.amzn1.x86_64"
+node_kernel_machine = node['kernel']['machine']   # "x86_64"
+node_ipaddress = node['ipaddress']
 
-  Chef::Log.info '########################################## cookbook: ' + cookbook_name + ' ################################'
+ec2 = node['ec2']
+if !ec2.nil?
+  node_public_hostname = node['ec2']['public_hostname']
+  node_public_ipv4 = node['ec2']['public_ipv4']
+  node_local_ipv4 = node['ec2']['local_ipv4']
+else
+  node_public_hostname = nil
+  node_public_ipv4 = nil
+  node_local_ipv4 = nil
+end
 
-  if node_public_hostname != nil
-    Chef::Log.info 'Node public host name:'+node_public_hostname
-  end
-  if node_public_ipv4 != nil
-    Chef::Log.info 'Node public IP Address:'+node_public_ipv4
-  end
-  if node_local_ipv4 != nil
-    Chef::Log.info 'Node local IP Address:'+node_local_ipv4
-  elsif node_ipaddress != nil
-    Chef::Log.info 'Node IP Address:'+node_ipaddress
-  end
-    
-  if !node_ipaddress.nil? && !node_local_ipv4.nil?
-    if node_local_ipv4 != node_ipaddress
-      if node_ipaddress != nil
-        Chef::Log.info 'Node IP Address:'+node_ipaddress
-      end
+Chef::Log.info '########################################## cookbook: ' + cookbook_name + ' ################################'
+
+if node_public_hostname != nil
+  Chef::Log.info 'Node public host name:'+node_public_hostname
+end
+if node_public_ipv4 != nil
+  Chef::Log.info 'Node public IP Address:'+node_public_ipv4
+end
+if node_local_ipv4 != nil
+  Chef::Log.info 'Node local IP Address:'+node_local_ipv4
+elsif node_ipaddress != nil
+  Chef::Log.info 'Node IP Address:'+node_ipaddress
+end
+
+if !node_ipaddress.nil? && !node_local_ipv4.nil?
+  if node_local_ipv4 != node_ipaddress
+    if node_ipaddress != nil
+      Chef::Log.info 'Node IP Address:'+node_ipaddress
     end
   end
-  
-  if !node_platform.nil? && !node_platform_version.nil? && !node_os.nil? && !node_os_version.nil? && !node_kernel_machine.nil?
-    Chef::Log.info 'Platform:' + node_platform + "  version:" + node_platform_version + "  os:" + node_os.to_s + "  os_version:" + node_os_version.to_s + '  machine:' + node_kernel_machine.to_s
-  end
+end
+
+if !node_platform.nil? && !node_platform_version.nil? && !node_os.nil? && !node_os_version.nil? && !node_kernel_machine.nil?
+  Chef::Log.info 'Platform:' + node_platform + "  version:" + node_platform_version + "  os:" + node_os.to_s + "  os_version:" + node_os_version.to_s + '  machine:' + node_kernel_machine.to_s
+end
